b743f2a2115ae78a8fa1b94834da9f099aaf588a
Jaewoo Jang <jaewoojang@Jaewoos-MacBook-Pro.local>
2021-01-04 10:45:53 +0900

    Rename tests.py to calculatorTest.py and define calculator class

---
+++ calculator.py
@@ -0,0 +1,14 @@
+import numpy
+
+
+class Calculator:
+
+    def __init__(self):
+        print("Welcome to the most basic calculator in the world")
+
+    # Adds too numbers
+    def add(self, x, y):
+        return x + y
+
+    def subtract(self, x, y):
+        return x - y
---
+++ calculatorTest.py
@@ -0,0 +1,30 @@
+import unittest
+import numpy
+from calculator import Calculator
+
+# Please refer to the following link to see more details
+# about 'unittest' module:
+# https://docs.python.org/3/library/unittest.html
+
+
+class CalculatorTest(unittest.TestCase):
+
+    def test_add(self):
+        x = 1
+        y = 1
+        testCalculator = Calculator()
+        testSum = testCalculator.add(x, y)
+
+        # Test the correctness of your code against the ground truth
+        self.assertEqual(testSum, 2)
+
+    def test_subtract(self):
+        x = 10
+        y = 5
+        testCalculator = Calculator()
+        testSum = testCalculator.subtract(x, y)
+        self.assertEqual(testSum, 5)
+
+
+if __name__ == '__main__':
+    unittest.main()
